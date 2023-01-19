e9f8e35543df2119ddb33ad129bfccdef2a9790f
Eunjae Lee <hey@eunjae.dev>
2023-01-19 15:50:54 +0100

    clean up outdated parts

--- packages/cli/templates/vue2/vite.config.ts
+++ packages/cli/templates/vue2/vite.config.ts
@@ -1,18 +1,13 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import { name } from "./package.json";
 
-console.log("💡 Field-Type name:", process.env.DEV_OVERRIDE_NAME ?? name);
+console.log("💡 Field-Type name:", name);
 console.log("");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    __DEV_OVERRIDE_NAME__: JSON.stringify(process.env.DEV_OVERRIDE_NAME),
-    __DEV_PLAYGROUND__: process.env.DEV_PLAYGROUND === "true",
-  },
   build: {
     // minify: false,
     rollupOptions: {
@@ -23,32 +18,10 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
-    vue(),
-    cssInjectedByJsPlugin(),
-    process.env.NODE_ENV === "production" && excludePlayground(),
-  ],
+  plugins: [vue(), cssInjectedByJsPlugin()],
   // @ts-expect-error this is coming from vitest
   test: {
     globals: true,
     environment: "jsdom",
-    alias: [
-      {
-        find: /^@storyblok\/field-type-common$/,
-        replacement: resolve(__dirname, "../common/src/index.ts"),
-      },
-    ],
   },
 });
-
-function excludePlayground() {
-  return {
-    name: "exclude-playground",
-    load(id: string) {
-      if (id.includes("playground/")) {
-        return "";
-      }
-      return null;
-    },
-  };
-}
