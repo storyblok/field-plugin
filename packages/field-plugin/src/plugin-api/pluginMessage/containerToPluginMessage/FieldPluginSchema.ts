43ff2995ee1fe3ffe145698047f8193cef0fcdaf
johannes-lindgren <johannes-lindgren@hotmail.com>
2023-01-22 15:52:18 +0100

    EXT-817: Adding missing is-functions for the plugin schema

--- packages/field-plugin/src/plugin-api/pluginMessage/containerToPluginMessage/FieldPluginSchema.ts
+++ packages/field-plugin/src/plugin-api/pluginMessage/containerToPluginMessage/FieldPluginSchema.ts
@@ -1,9 +1,24 @@
 /**
  * The block schema that describes a custom field
  */
+import { hasKey } from '../../../utils'
+
 export type FieldPluginSchema = {
   field_type: string
   options: FieldPluginOption[]
 }
 
 export type FieldPluginOption = { name: string; value: string }
+
+export const isFieldPluginOption = (it: unknown): it is FieldPluginOption =>
+  hasKey(it, 'name') &&
+  typeof it.name === 'string' &&
+  hasKey(it, 'value') &&
+  typeof it.value === 'string'
+
+export const isFieldPluginSchema = (it: unknown): it is FieldPluginSchema =>
+  hasKey(it, 'field_type') &&
+  typeof it.field_type === 'string' &&
+  hasKey(it, 'options') &&
+  Array.isArray(it.options) &&
+  it.options.every(isFieldPluginOption)
