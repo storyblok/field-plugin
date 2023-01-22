40d6f113d6348f865224cc1f6c9ac356720bbd01
johannes-lindgren <johannes-lindgren@hotmail.com>
2023-01-22 15:46:19 +0100

    EXT-817: Renamed recordFromFieldPluginOptions and moved to the containerToPluginMessage module

--- packages/field-plugin/src/plugin-api/pluginMessage/containerToPluginMessage/recordFromFieldPluginOptions.ts
+++ packages/field-plugin/src/plugin-api/pluginMessage/containerToPluginMessage/recordFromFieldPluginOptions.ts
@@ -1,6 +1,6 @@
-import { FieldPluginOption } from '../../../plugin-api'
+import { FieldPluginOption } from '../../index'
 
-export const recordFromOptions = (
+export const recordFromFieldPluginOptions = (
   options: FieldPluginOption[],
 ): Record<string, string> =>
   options.reduce((options, option) => {
