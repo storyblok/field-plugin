c3e15e942a63653149d09ac19890934a3d9e39be
Eunjae Lee <hey@eunjae.dev>
2023-01-23 13:21:32 +0100

    chore: auto-format cli code

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,17 +1,17 @@
-import { generate, TEMPLATES } from './generate';
-import { deploy, type DeployArgs } from './deploy';
-import { newProject } from './newProject';
-import { Command } from 'commander';
+import { generate, TEMPLATES } from './generate'
+import { deploy, type DeployArgs } from './deploy'
+import { newProject } from './newProject'
+import { Command } from 'commander'
 
-const program = new Command();
+const program = new Command()
 
 export const main = () => {
   program
     .command('new', { isDefault: true })
     .description('creates a new repository to start developing field plugins')
     .action(function () {
-      newProject();
-    });
+      newProject()
+    })
 
   program
     .command('deploy')
@@ -21,34 +21,34 @@ export const main = () => {
     .action(async function (this: Command) {
       // eslint-disable-next-line functional/no-this-expression
       const { name, skipPrompts } = this.opts<{
-        name?: string;
-        skipPrompts?: boolean;
-      }>();
-      await deploy({ fieldPluginName: name, skipPrompts } as DeployArgs);
-    });
+        name?: string
+        skipPrompts?: boolean
+      }>()
+      await deploy({ fieldPluginName: name, skipPrompts } as DeployArgs)
+    })
 
   const templateOptions = TEMPLATES.map(
-    (template) => `'${template.value}'`
-  ).join(' | ');
+    (template) => `'${template.value}'`,
+  ).join(' | ')
   program
     .command('generate')
     .description('generates new field-plugin inside your project')
     .option(
       '--template <value>',
-      `name of template to use (${templateOptions})`
+      `name of template to use (${templateOptions})`,
     )
     .option(
       '--name <value>',
-      'name of plugin (Lowercase alphanumeric and dash)'
+      'name of plugin (Lowercase alphanumeric and dash)',
     )
     .action(async function (this: Command) {
       // eslint-disable-next-line functional/no-this-expression
       const { name, template } = this.opts<{
-        name?: string;
-        template?: string;
-      }>();
-      await generate({ packageName: name, template });
-    });
+        name?: string
+        template?: string
+      }>()
+      await generate({ packageName: name, template })
+    })
 
-  program.parse(process.argv);
-};
+  program.parse(process.argv)
+}
