5681d2df94df918fdc86a1b03711b69a99829933
Eunjae Lee <hey@eunjae.dev>
2023-01-23 13:12:59 +0100

    chore: add .prettierrc (#3)
    * chore: add .prettierrc
    
    * remove duplicated prettier config from eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,10 @@
-const numberFromStringMessage = (forbiddenToken) => `"Usage of ${forbiddenToken} is forbidden. Create one utility function "numberFromString()" that 1) parses a number from a string with Number(), 2) adds an exception to the eslint rule, 3) checks for NaN with Number.isNaN(), 4) checks for infinities with Number.isFinite(), 4) return the parsed number or undefined if the checks failed.`
+const numberFromStringMessage = (forbiddenToken) =>
+  `"Usage of ${forbiddenToken} is forbidden. Create one utility function "numberFromString()" that 1) parses a number from a string with Number(), 2) adds an exception to the eslint rule, 3) checks for NaN with Number.isNaN(), 4) checks for infinities with Number.isFinite(), 4) return the parsed number or undefined if the checks failed.`
 
 module.exports = {
   root: true,
-  extends: [
-    'prettier',
-    'eslint:recommended',
-  ],
-  plugins: [
-    'prettier',
-    'functional',
-  ],
+  extends: ['prettier', 'eslint:recommended'],
+  plugins: ['prettier', 'functional'],
   parser: '@typescript-eslint/parser',
   overrides: [
     // For parsing ts files with type information
@@ -25,32 +20,30 @@ module.exports = {
       ],
       parserOptions: {
         tsconfigRootDir: __dirname,
-        project: [
-          './tsconfig.eslint.json',
-          './packages/*/tsconfig.json'
-        ],
+        project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
       },
-      plugins: [
-        '@typescript-eslint',
-      ],
+      plugins: ['@typescript-eslint'],
       rules: {
         '@typescript-eslint/restrict-plus-operands': 'error',
         '@typescript-eslint/no-misused-promises': 'off',
         '@typescript-eslint/no-explicit-any': 'error',
-        '@typescript-eslint/no-unused-vars': ['warn', {
-          args: "none",
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_'
-        }],
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            args: 'none',
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+          },
+        ],
       },
     },
     {
-      files: ["*.test.ts", "*.test.tsx"],
+      files: ['*.test.ts', '*.test.tsx'],
       env: {
         jest: true,
         node: true,
-      }
+      },
     },
     {
       files: ['packages/cli/**'],
@@ -62,12 +55,15 @@ module.exports = {
   rules: {
     'no-warning-comments': 'warn',
     'use-isnan': 'error',
-    'eqeqeq': 'error',
+    eqeqeq: 'error',
     'no-var': 'error',
     'no-bitwise': 'error',
-    'no-console': ['warn', {
-      allow: ['warn', 'error'],
-    }],
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
     'no-inline-comments': 'warn',
     'no-param-reassign': 'error',
     'no-implicit-coercion': 'error',
@@ -87,41 +83,34 @@ module.exports = {
         caughtErrorsIgnorePattern: '^_',
       },
     ],
-    'prettier/prettier': ['warn', {
-      bracketSpacing: true,
-      printWidth: 80,
-      semi: false,
-      singleQuote: true,
-      trailingComma: 'all',
-      singleAttributePerLine: true
-    }],
-    "yoda": 'warn',
-    "no-restricted-properties": [
+    'prettier/prettier': ['warn'],
+    yoda: 'warn',
+    'no-restricted-properties': [
       'error',
       {
-        "object": "Number",
-        "property": "parseInt",
-        "message": numberFromStringMessage('Number.parseInt()')
+        object: 'Number',
+        property: 'parseInt',
+        message: numberFromStringMessage('Number.parseInt()'),
       },
       {
-        "object": "Number",
-        "property": "parseFloat",
-        "message": numberFromStringMessage('Number.parseFloat()')
-      }
+        object: 'Number',
+        property: 'parseFloat',
+        message: numberFromStringMessage('Number.parseFloat()'),
+      },
     ],
-    "no-restricted-syntax": [
-      "error",
+    'no-restricted-syntax': [
+      'error',
       {
-        "selector": "CallExpression[callee.name='parseInt']",
-        "message": numberFromStringMessage('parseInt()')
+        selector: "CallExpression[callee.name='parseInt']",
+        message: numberFromStringMessage('parseInt()'),
       },
       {
-        "selector": "CallExpression[callee.name='parseFloat']",
-        "message": numberFromStringMessage('parseFloat()')
+        selector: "CallExpression[callee.name='parseFloat']",
+        message: numberFromStringMessage('parseFloat()'),
       },
       {
-        "selector": "CallExpression[callee.name='Number']",
-        "message": numberFromStringMessage('Number()'),
+        selector: "CallExpression[callee.name='Number']",
+        message: numberFromStringMessage('Number()'),
       },
       // Function expressions are useful in a few contexts of higher order components when the function needs to be named.
       // {
@@ -129,9 +118,9 @@ module.exports = {
       //   "message": "Prefer arrow functions over function expressions."
       // },
       {
-        "selector": "FunctionDeclaration",
-        "message": "Prefer arrow functions over function declarations."
-      }
-    ]
+        selector: 'FunctionDeclaration',
+        message: 'Prefer arrow functions over function declarations.',
+      },
+    ],
   },
 }
