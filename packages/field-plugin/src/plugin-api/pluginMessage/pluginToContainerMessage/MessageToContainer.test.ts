d96dea836380bf371e368f8078d2fc62929896cc
johannes-lindgren <johannes-lindgren@hotmail.com>
2023-02-13 11:26:18 +0100

    Removed asset selector.

--- packages/field-plugin/src/plugin-api/pluginMessage/pluginToContainerMessage/MessageToContainer.test.ts
+++ packages/field-plugin/src/plugin-api/pluginMessage/pluginToContainerMessage/MessageToContainer.test.ts
@@ -1,114 +1,118 @@
 import { isMessageToContainer, MessageToContainer } from './MessageToContainer'
 
-const stub: MessageToContainer<'dummy'> = {
+const uid = '-preview-abc-123'
+const event = 'dummy-event-3478'
+const stub: MessageToContainer<typeof event> = {
   action: 'plugin-changed',
-  uid: '-preview',
-  event: 'dummy',
+  uid,
+  event,
 }
 
 describe('message from plugin to container', () => {
-  describe('the action property', () => {
-    it('is "plugin-changed"', () => {
-      expect(
-        isMessageToContainer({
-          ...stub,
-          action: 'plugin-changed',
-        }),
-      ).toBeTruthy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          action: 'something-different',
-        }),
-      ).toBeFalsy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          action: undefined,
-        }),
-      ).toBeFalsy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          action: 123,
-        }),
-      ).toBeFalsy()
+  describe('validator', () => {
+    describe('the action property', () => {
+      it('is "plugin-changed"', () => {
+        expect(
+          isMessageToContainer({
+            ...stub,
+            action: 'plugin-changed',
+          }),
+        ).toBeTruthy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            action: 'something-different',
+          }),
+        ).toBeFalsy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            action: undefined,
+          }),
+        ).toBeFalsy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            action: 123,
+          }),
+        ).toBeFalsy()
+      })
     })
-  })
-  describe('the uid property', () => {
-    it('requires uid', () => {
-      expect(
-        isMessageToContainer({
-          ...stub,
-          uid: 'abc',
-        }),
-      ).toBeTruthy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          uid: undefined,
-        }),
-      ).toBeFalsy()
+    describe('the uid property', () => {
+      it('requires uid', () => {
+        expect(
+          isMessageToContainer({
+            ...stub,
+            uid: 'abc',
+          }),
+        ).toBeTruthy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            uid: undefined,
+          }),
+        ).toBeFalsy()
+      })
+      it('requires uid to be a string', () => {
+        expect(
+          isMessageToContainer({
+            ...stub,
+            uid: 'abc',
+          }),
+        ).toBeTruthy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            uid: undefined,
+          }),
+        ).toBeFalsy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            uid: 123,
+          }),
+        ).toBeFalsy()
+      })
     })
-    it('requires uid to be a string', () => {
-      expect(
-        isMessageToContainer({
-          ...stub,
-          uid: 'abc',
-        }),
-      ).toBeTruthy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          uid: undefined,
-        }),
-      ).toBeFalsy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          uid: 123,
-        }),
-      ).toBeFalsy()
-    })
-  })
-  describe('the event property', () => {
-    it('is a string', () => {
-      expect(
-        isMessageToContainer({
-          ...stub,
-          event: 'any-string',
-        }),
-      ).toBeTruthy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          event: 123,
-        }),
-      ).toBeFalsy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          event: undefined,
-        }),
-      ).toBeFalsy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          event: true,
-        }),
-      ).toBeFalsy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          event: false,
-        }),
-      ).toBeFalsy()
-      expect(
-        isMessageToContainer({
-          ...stub,
-          event: null,
-        }),
-      ).toBeFalsy()
+    describe('the event property', () => {
+      it('is a string', () => {
+        expect(
+          isMessageToContainer({
+            ...stub,
+            event: 'any-string',
+          }),
+        ).toBeTruthy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            event: 123,
+          }),
+        ).toBeFalsy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            event: undefined,
+          }),
+        ).toBeFalsy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            event: true,
+          }),
+        ).toBeFalsy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            event: false,
+          }),
+        ).toBeFalsy()
+        expect(
+          isMessageToContainer({
+            ...stub,
+            event: null,
+          }),
+        ).toBeFalsy()
+      })
     })
   })
 })
--- packages/field-plugin/src/plugin-api/pluginMessage/pluginToContainerMessage/ModalChangeMessage.test.ts
+++ packages/field-plugin/src/plugin-api/pluginMessage/pluginToContainerMessage/ModalChangeMessage.test.ts
@@ -1,81 +1,97 @@
 import { isMessageToContainer } from './MessageToContainer'
-import { isModalChangeMessage, ModalChangeMessage } from './ModalChangeMessage'
+import {
+  isModalChangeMessage,
+  modalChangeMessage,
+  ModalChangeMessage,
+} from './ModalChangeMessage'
 
+const uid = '-preview-abc-123'
 const stub: ModalChangeMessage = {
   action: 'plugin-changed',
   event: 'toggleModal',
-  uid: '-prevew',
+  uid,
   status: true,
 }
 
 describe('ModalChangeMessage', () => {
-  it('is a MessageToContainer', () => {
-    expect(isMessageToContainer(stub)).toBeTruthy()
+  describe('validator', () => {
+    it('is a MessageToContainer', () => {
+      expect(isMessageToContainer(stub)).toBeTruthy()
+    })
+    it('requires event to be "toggleModal"', () => {
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          event: 'toggleModal',
+        }),
+      ).toBeTruthy()
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          event: 'somethingElse',
+        }),
+      ).toBeFalsy()
+    })
+    it('has a status', () => {
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          status: true,
+        }),
+      ).toBeTruthy()
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          status: undefined,
+        }),
+      ).toBeFalsy()
+    })
+    test('that the status is a boolean', () => {
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          status: true,
+        }),
+      ).toBeTruthy()
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          status: false,
+        }),
+      ).toBeTruthy()
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          status: 'false',
+        }),
+      ).toBeFalsy()
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          status: 123,
+        }),
+      ).toBeFalsy()
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          status: null,
+        }),
+      ).toBeFalsy()
+      expect(
+        isModalChangeMessage({
+          ...stub,
+          status: undefined,
+        }),
+      ).toBeFalsy()
+    })
   })
-  it('requires event to be "toggleModal"', () => {
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        event: 'toggleModal',
-      }),
-    ).toBeTruthy()
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        event: 'somethingElse',
-      }),
-    ).toBeFalsy()
-  })
-  it('has a status', () => {
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        status: true,
-      }),
-    ).toBeTruthy()
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        status: undefined,
-      }),
-    ).toBeFalsy()
-  })
-  test('that the status is a boolean', () => {
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        status: true,
-      }),
-    ).toBeTruthy()
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        status: false,
-      }),
-    ).toBeTruthy()
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        status: 'false',
-      }),
-    ).toBeFalsy()
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        status: 123,
-      }),
-    ).toBeFalsy()
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        status: null,
-      }),
-    ).toBeFalsy()
-    expect(
-      isModalChangeMessage({
-        ...stub,
-        status: undefined,
-      }),
-    ).toBeFalsy()
+  describe('constructor', () => {
+    it('includes the uid', () => {
+      expect(modalChangeMessage(uid, true)).toHaveProperty('uid', uid)
+    })
+    it('includes the modal status', () => {
+      expect(modalChangeMessage(uid, true)).toHaveProperty('status', true)
+      expect(modalChangeMessage(uid, false)).toHaveProperty('status', false)
+    })
   })
 })
