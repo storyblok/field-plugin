512f3f8ca0082e06f9aa8461237c18eff839db76
Eunjae Lee <hey@eunjae.dev>
2023-03-09 11:39:27 +0100

    EXT-1167: replace the zipped dependencies with the ones from NPM (#59)
    * EXT-1167: replace the zipped dependencies with the ones from NPM
    
    * uncomment

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -82,7 +82,7 @@ export const add: AddFunc = async (args) => {
     mkdirSync(dirname(destFilePath), {
       recursive: true,
     })
-    if (basename(file) === 'package.json') {
+    if (file === resolve(templatePath, 'package.json')) {
       const packageJson = JSON.parse(readFileSync(file).toString()) as Record<
         string,
         unknown
