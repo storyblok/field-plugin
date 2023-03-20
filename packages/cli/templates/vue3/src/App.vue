f3ee17264b91d577e64c856d1c9512a69fd1090e
Eunjae Lee <hey@eunjae.dev>
2023-03-20 17:15:32 +0100

    first iteration

--- packages/cli/templates/vue3/src/App.vue
+++ packages/cli/templates/vue3/src/App.vue
@@ -1,17 +1,54 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { useFieldPlugin } from './useFieldPlugin'
+
+const plugin = useFieldPlugin()
+const imageUrl = ref('')
+
+const handleIncrement = () => {
+  plugin.value.actions?.setValue(
+    (typeof plugin.value.data.value === 'number'
+      ? plugin.value.data.value
+      : 0) + 1,
+  )
+}
+
+const toggleModal = () => {
+  plugin.value.actions?.setModalOpen(!plugin.value.data.isModalOpen)
+}
+
+const handleSelectAsset = () => {
+  plugin.value.actions?.selectAsset((filename) => (imageUrl.value = filename))
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div v-if="plugin.type === 'loading'">Loading...</div>
+    <div v-else-if="plugin.type === 'error'">Error</div>
+    <div v-else>
+      <div>
+        <span
+          >Value:
+          {{
+            typeof plugin.data?.value !== 'number'
+              ? 0
+              : JSON.stringify(plugin.data.value)
+          }}</span
+        >
+        <button @click="handleIncrement">Increment</button>
+      </div>
+      <div>
+        <button @click="toggleModal">
+          {{ plugin.data.isModalOpen ? 'Close' : 'Open' }} modal
+        </button>
+      </div>
+      <div>
+        <button @click="handleSelectAsset">Select Asset</button>
+        <span>Image Url: {{ imageUrl }}</span>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
