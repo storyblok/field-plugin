7d8c5ee68d97063436b394a67acc3d359e3a4c01
Bibiana Sebestianova <b.sebestianova@gmail.com>
2023-03-20 15:10:12 +0100

    EXT-894: added functionality - open/close modal - added asset selector

--- packages/cli/templates/react/src/App.tsx
+++ packages/cli/templates/react/src/App.tsx
@@ -1,10 +1,20 @@
 import {useFieldPlugin} from "./useFieldPlugin";
+import {useState} from "react";
 
 function App() {
     const {type, data, actions} = useFieldPlugin()
+    const [imageUrl, setImageUrl] = useState<string>('');
 
     const handleIncrement = () => {
         actions?.setValue((typeof data.value === 'number' ? data.value : 0) + 1)
+    }
+
+    const handleToggleModal = (isOpen: boolean) => {
+        actions?.setModalOpen(isOpen)
+    }
+
+    const handleSelectAsset = () => {
+        actions?.selectAsset((filename) => setImageUrl(filename))
     }
 
 
@@ -25,11 +35,23 @@ function App() {
 
     return (
         <div>
-            <span>Value: {label}</span>
-            <button onClick={handleIncrement}>
-                Increment
-            </button>
+            <div>
+                <span>Value: {label}</span>
+                <button onClick={handleIncrement}>
+                    Increment
+                </button>
+            </div>
+            <div>
+                <button
+                    onClick={() => handleToggleModal(!data.isModalOpen)}>{data.isModalOpen ? 'Close' : 'Open'} modal
+                </button>
+            </div>
+            <div>
+                <button onClick={handleSelectAsset}>Select Asset</button>
+                <span>Image Url: {imageUrl}</span>
+            </div>
         </div>
+
     )
 }
 
