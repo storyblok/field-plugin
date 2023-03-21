0fc8fa7c77bc95e2534e557b48bd256e46084667
Eunjae Lee <hey@eunjae.dev>
2023-03-21 15:09:41 +0100

    update build config

--- packages/cli/templates/vue3/vite.config.ts
+++ packages/cli/templates/vue3/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
   server: {
     port: 8080,
     host: true,
+  },
+  build: {
+    minify: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
   },
 })
