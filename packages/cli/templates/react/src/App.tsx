c8cd873c9c0dc21a17495a73ba6af3c92a15aff1
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-03-28 14:43:54 +0200

    Format React Template with Prettier

--- packages/cli/templates/react/src/App.tsx
+++ packages/cli/templates/react/src/App.tsx
@@ -1,26 +1,35 @@
-import {useFieldPlugin} from './useFieldPlugin'
+import { useFieldPlugin } from './useFieldPlugin'
 import Counter from './components/Counter'
 import ModalToggle from './components/ModalToggle'
 import AssetSelector from './components/AssetSelector'
 
 function App() {
-    const {type, data, actions} = useFieldPlugin()
+  const { type, data, actions } = useFieldPlugin()
 
-    if (type === 'loading') {
-        return <span>Loading...</span>
-    }
+  if (type === 'loading') {
+    return <span>Loading...</span>
+  }
 
-    if (type === 'error' || typeof data === 'undefined' && typeof actions === 'undefined') {
-        return <span>Error</span>
-    }
+  if (
+    type === 'error' ||
+    (typeof data === 'undefined' && typeof actions === 'undefined')
+  ) {
+    return <span>Error</span>
+  }
 
-    return (
-        <div className="field-plugin">
-            <ModalToggle isModalOpen={data.isModalOpen} setModalOpen={actions.setModalOpen}/>
-            <Counter setValue={actions.setValue} value={data.value}/>
-            <AssetSelector selectAsset={actions.selectAsset}/>
-        </div>
-    )
+  return (
+    <div className="field-plugin">
+      <ModalToggle
+        isModalOpen={data.isModalOpen}
+        setModalOpen={actions.setModalOpen}
+      />
+      <Counter
+        setValue={actions.setValue}
+        value={data.value}
+      />
+      <AssetSelector selectAsset={actions.selectAsset} />
+    </div>
+  )
 }
 
 export default App
--- packages/cli/templates/react/src/components/AssetSelector.tsx
+++ packages/cli/templates/react/src/components/AssetSelector.tsx
@@ -1,23 +1,23 @@
-import {FunctionComponent, useState} from 'react'
-import {SelectAsset} from "@storyblok/field-plugin";
-import Button from "./button/Button";
+import { FunctionComponent, useState } from 'react'
+import { SelectAsset } from '@storyblok/field-plugin'
+import Button from './button/Button'
 
 type AssetSelectorFunc = FunctionComponent<{
-    selectAsset: SelectAsset
+  selectAsset: SelectAsset
 }>
-const AssetSelector: AssetSelectorFunc = ({selectAsset}) => {
-    const [imageUrl, setImageUrl] = useState<string>('')
+const AssetSelector: AssetSelectorFunc = ({ selectAsset }) => {
+  const [imageUrl, setImageUrl] = useState<string>('')
 
-    const handleSelectAsset = async () => {
-        setImageUrl(await selectAsset())
-    }
+  const handleSelectAsset = async () => {
+    setImageUrl(await selectAsset())
+  }
 
-    return (
-        <div className="asset-selector">
-            <Button onClick={handleSelectAsset}>Select Asset</Button>
-            <span>Image Url: {imageUrl}</span>
-        </div>
-    )
+  return (
+    <div className="asset-selector">
+      <Button onClick={handleSelectAsset}>Select Asset</Button>
+      <span>Image Url: {imageUrl}</span>
+    </div>
+  )
 }
 
 export default AssetSelector
