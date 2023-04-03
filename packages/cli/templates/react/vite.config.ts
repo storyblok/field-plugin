3c15de63ce9fada31cedf5df569fcaf7597dfbac
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-03 14:40:40 +0200

    Rolling back accidental commit

--- packages/cli/templates/react/vite.config.ts
+++ packages/cli/templates/react/vite.config.ts
@@ -2,32 +2,9 @@ import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
-const plugin = (): PluginOption => ({
-  name: 'field-plugin-print',
-  watchChange: () => {
-    console.log('watching')
-  },
-  configureServer: (server) => {
-    console.log('Gello')
-  },
-  buildStart: () => {
-    console.log()
-    console.log()
-    console.log('Deploy to Storyblok with')
-    console.log('\n > npx @storyblok/field-plugin-cli deploy\n')
-    console.log(
-      'Personal access tokens (for deployment): https://app.storyblok.com/#/me/account?tab=token',
-    )
-    console.log(
-      'Partner Portal plugins: https://app.storyblok.com/#/partner/fields',
-    )
-    console.log('Personal plugins: https://app.storyblok.com/#/me/plugins')
-  },
-})
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin(), plugin()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     rollupOptions: {
       output: {
