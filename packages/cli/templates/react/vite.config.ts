6144b36877b62f9624c58283a5b381a0ed4e541b
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-03 14:34:31 +0200

    Using field plugin from npm instead of workspace

--- packages/cli/templates/react/vite.config.ts
+++ packages/cli/templates/react/vite.config.ts
@@ -1,10 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
+const plugin = (): PluginOption => ({
+  name: 'field-plugin-print',
+  watchChange: () => {
+    console.log('watching')
+  },
+  configureServer: (server) => {
+    console.log('Gello')
+  },
+  buildStart: () => {
+    console.log()
+    console.log()
+    console.log('Deploy to Storyblok with')
+    console.log('\n > npx @storyblok/field-plugin-cli deploy\n')
+    console.log(
+      'Personal access tokens (for deployment): https://app.storyblok.com/#/me/account?tab=token',
+    )
+    console.log(
+      'Partner Portal plugins: https://app.storyblok.com/#/partner/fields',
+    )
+    console.log('Personal plugins: https://app.storyblok.com/#/me/plugins')
+  },
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
+  plugins: [react(), cssInjectedByJsPlugin(), plugin()],
   build: {
     rollupOptions: {
       output: {
