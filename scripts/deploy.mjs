897bdb3d789b8ee3db130787a3d45702902411ae
Eunjae Lee <hey@eunjae.dev>
2023-04-07 17:45:46 +0200

    EXT-1287: add deploy script for field-plugin and cli

---
+++ scripts/deploy.mjs
@@ -0,0 +1,128 @@
+#!/usr/bin/env zx
+
+import { $, which } from 'zx'
+import prompts from 'prompts'
+import semver from 'semver'
+import { readFileSync } from 'fs'
+import { bold, green, red } from 'kleur/colors'
+
+const print = (...args) => {
+  // eslint-disable-next-line no-undef, no-console
+  console.log(...args)
+}
+
+const exit = (code) => {
+  // eslint-disable-next-line no-undef
+  process.exit(code)
+}
+
+const TARGETS = [
+  {
+    title: 'Library',
+    value: 'field-plugin',
+  },
+  {
+    title: 'CLI',
+    value: 'cli',
+  },
+]
+
+// Check if `gh` exists
+if (!(await which('gh', { nothrow: true }))) {
+  print(
+    bold(red('[Error]')),
+    'Cannot find `gh` command. Please install it via the following command:',
+  )
+  print('  > brew install gh')
+  print('')
+  print('or visit https://cli.github.com/')
+  exit(1)
+}
+
+// Check if authenticated with `gh`
+try {
+  $`gh auth status`
+} catch (processOutput) {
+  if (processOutput.exitCode === 1) {
+    print(
+      bold(red('[Error]')),
+      'You need to authenticate with `gh`. Run the following command:',
+    )
+    print('  > gh auth login')
+  }
+}
+
+// Check if the current branch is `main`
+const currentBranch = await $`git branch --show-current`
+if (currentBranch.toString().trim() !== 'main') {
+  print(bold(red('[Error]')), 'This command runs only from `main` branch.')
+  exit(1)
+}
+
+// Check if the working directory is clean
+try {
+  await $`git diff-index --quiet HEAD --`
+} catch (processOutput) {
+  if (processOutput.exitCode === 1) {
+    print(
+      bold(red('[Error]')),
+      'There are uncommitted changes in the working directory. Please clean them up before proceeding.',
+    )
+    exit(1)
+  }
+}
+
+// Select which package to deploy ('field-plugin' | 'cli')
+const { packageFolder } = await prompts({
+  type: 'select',
+  name: 'packageFolder',
+  message: 'What to deploy?',
+  choices: TARGETS,
+})
+
+// Get the current version
+const { version: currentVersion } = JSON.parse(
+  readFileSync(`packages/${packageFolder}/package.json`).toString(),
+)
+print(bold(green('✔')), bold('Current version ›'), currentVersion)
+
+const prerelease = semver.prerelease(currentVersion)
+
+// Get the next version
+// eslint-disable-next-line functional/no-let
+let nextVersion
+if (prerelease) {
+  // e.g. prerelease === ['alpha', 8]
+  const result = await prompts({
+    type: 'text',
+    name: 'nextVersion',
+    message: 'Next version?',
+    initial: semver.inc(currentVersion, 'prerelease', prerelease[0]),
+  })
+  nextVersion = result.nextVersion
+} else {
+  const { incrementLevel } = await prompts({
+    type: 'select',
+    name: 'incrementLevel',
+    message: 'Increment Level?',
+    choices: [{ value: 'patch' }, { value: 'minor' }, { value: 'major' }],
+  })
+
+  const result = await prompts({
+    type: 'text',
+    name: 'nextVersion',
+    message: 'Next version?',
+    initial: semver.inc(currentVersion, incrementLevel),
+  })
+  nextVersion = result.nextVersion
+}
+
+// Check out to a release branch and create a pull-request
+const branchName = `chore/release-${packageFolder}-${nextVersion}`
+await $`git checkout -b ${branchName}`
+await $`git add .`
+const commitMessage = `chore: release ${packageFolder}@${nextVersion}`
+await $`git commit -m ${commitMessage}`
+await $`git push -u origin ${branchName}`
+await $`gh pr create --fill`
+await $`gh pr view --web`
