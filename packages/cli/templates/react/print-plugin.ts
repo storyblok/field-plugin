fde728421dd8bdf88bc8c11cb2df62201deb37d7
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-10 10:13:32 +0200

    feat(cli): added vite plugin that during build prints links to the sandbox and to the editors.

---
+++ packages/cli/templates/react/print-plugin.ts
@@ -0,0 +1,25 @@
+import { PluginOption } from 'vite'
+
+const printPlugin = (): PluginOption => ({
+  name: 'storyblok-field-plugin',
+  writeBundle: () => {
+    console.log(`
+    To deploy to Storyblok, run
+    
+      > yarn deploy
+    `)
+  },
+  buildStart: () => {
+    console.log(`
+    Partner Portal plugins:
+      - https://app.storyblok.com/#/partner/fields
+    Personal plugins:
+      - https://app.storyblok.com/#/me/plugins
+    
+    Try out the app with the Field Plugin Sandbox:
+      - https://storyblok-field-plugin-sandbox.vercel.app
+    `)
+  },
+})
+
+export default printPlugin
--- packages/cli/templates/react/vite.config.ts
+++ packages/cli/templates/react/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig, PluginOption } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import cssInjectedByJs from 'vite-plugin-css-injected-by-js'
+import printPlugin from './print-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
+  plugins: [react(), cssInjectedByJs(), printPlugin()],
   build: {
     rollupOptions: {
       output: {
