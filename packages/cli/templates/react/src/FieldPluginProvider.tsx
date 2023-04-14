0b276ddfb7397a07758d4dc4ce8f698aed75f669
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-14 11:27:48 +0200

    fix(cli): Removing conditional that cannot occur. Also avoiding null whenever possible, in favor of undefined.

--- packages/cli/templates/react/src/FieldPluginProvider.tsx
+++ packages/cli/templates/react/src/FieldPluginProvider.tsx
@@ -11,9 +11,9 @@ import {
 } from '@storyblok/field-plugin'
 import { ReactNode } from 'react'
 
-export const FieldPluginContext = createContext<FieldPluginResponse | null>(
-  null,
-)
+export const FieldPluginContext = createContext<
+  Extract<FieldPluginResponse, { type: 'loaded' }> | undefined
+>(undefined)
 
 type Props = {
   Error?: ComponentType<{ error: Error }>
@@ -30,14 +30,12 @@ export const FieldPluginProvider: FunctionComponent<Props> = ({
     type: 'loading',
   })
 
-  useEffect(() => {
-    return createFieldPlugin(setState)
-  }, [])
+  useEffect(() => createFieldPlugin(setState), [])
 
   if (state.type === 'loading') {
-    return Loading ? <Loading /> : null
+    return Loading ? <Loading /> : <></>
   } else if (state.type === 'error') {
-    return Error ? <Error error={state.error} /> : null
+    return Error ? <Error error={state.error} /> : <></>
   } else {
     return (
       <FieldPluginContext.Provider value={state}>
--- packages/cli/templates/react/src/useFieldPlugin.ts
+++ packages/cli/templates/react/src/useFieldPlugin.ts
@@ -2,18 +2,16 @@ import { FieldPluginResponse } from '@storyblok/field-plugin'
 import { useContext } from 'react'
 import { FieldPluginContext } from './FieldPluginProvider'
 
-export const useFieldPlugin = () => {
+export const useFieldPlugin = (): Extract<
+  FieldPluginResponse,
+  { type: 'loaded' }
+> => {
   const plugin = useContext(FieldPluginContext)
-  if (plugin === null) {
-    throw new Error(
-      `You need to wrap your app with \`<FieldPluginProvider>\` component.`,
-    )
-  }
-
-  if (plugin.type !== 'loaded') {
+  if (!plugin) {
     throw new Error(
       'The plugin is not loaded, yet `useFieldPlugin()` was invoked. Ensure that the component that invoked `useFieldPlugin()` is wrapped within `<FieldPluginProvider>`.',
     )
   }
-  return plugin as Extract<FieldPluginResponse, { type: 'loaded' }>
+
+  return plugin
 }
