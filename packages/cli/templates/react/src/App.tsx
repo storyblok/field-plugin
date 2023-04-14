17ec2e401d4d0466ad844bdeb1b36b1f85d9e7e9
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-14 10:57:30 +0200

    fix(cli): Extracting Loading and Error to preserve referential equality.

--- packages/cli/templates/react/src/App.tsx
+++ packages/cli/templates/react/src/App.tsx
@@ -5,12 +5,17 @@ import { FieldPluginProvider } from './FieldPluginProvider'
 const App: FunctionComponent = () => {
   return (
     <FieldPluginProvider
-      Loading={() => <p>Loading...</p>}
-      Error={({ error }) => <p>Error: {error.message}</p>}
+      Loading={Loading}
+      Error={Error}
     >
       <FieldPlugin />
     </FieldPluginProvider>
   )
 }
 
+const Loading: FunctionComponent = () => <p>Loading...</p>
+const Error: FunctionComponent<{ error: Error }> = (props) => {
+  console.error(props.error)
+  return <p>An error occured, please see the console for more details.</p>
+}
 export default App
