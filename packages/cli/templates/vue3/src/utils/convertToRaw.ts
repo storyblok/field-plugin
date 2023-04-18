bf976f315ab4b29fff684ceeaf0434a536d6aa35
Eunjae Lee <hey@eunjae.dev>
2023-04-18 15:44:07 +0200

    EXT-1387: convert value to raw object for setValue in vue 3 template (#131)
    * EXT-1387: unproxy data to setValue in vue 3 template
    
    * clean up
    
    * support array
    
    * do not replace the whole data reference

---
+++ packages/cli/templates/vue3/src/utils/convertToRaw.ts
@@ -0,0 +1,26 @@
+import { toRaw, isProxy, isRef, unref } from 'vue'
+
+export function convertToRaw(value: any) {
+  let rawValue = value
+  if (isProxy(rawValue)) {
+    rawValue = toRaw(rawValue)
+  }
+  if (isRef(rawValue)) {
+    rawValue = unref(rawValue)
+  }
+
+  if (isObject(rawValue)) {
+    return Object.keys(rawValue).reduce((result, key) => {
+      result[key] = convertToRaw(rawValue[key])
+      return result
+    }, rawValue)
+  } else if (Array.isArray(rawValue)) {
+    return rawValue.map(convertToRaw)
+  } else {
+    return rawValue
+  }
+}
+
+function isObject(value: unknown) {
+  return Object.prototype.toString.call(value) === '[object Object]'
+}
