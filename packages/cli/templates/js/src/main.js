bc0b74526686cc86ac79c1df1c90ad2cba201d1c
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-25 12:37:02 +0200

    Nicer error message from failing fieldtype-wrapper.js

--- packages/cli/templates/js/src/main.js
+++ packages/cli/templates/js/src/main.js
@@ -13,3 +13,8 @@ createFieldPlugin((response) => {
   buttonEl.textContent = `Increment: ${data.value ?? 0}`
   buttonEl.onclick = () => actions.setValue((data.value ?? 0) + 1)
 })
+
+// This error replaces another error which message is harder to understand and impossible to avoid util the issue https://github.com/storyblok/field-plugin/issues/107 has been resolved.
+throw new Error(
+  `This error can be safely ignored. It is caused by the legacy field plugin API. See issue https://github.com/storyblok/field-plugin/issues/107`,
+)
--- packages/cli/templates/vue2/src/main.ts
+++ packages/cli/templates/vue2/src/main.ts
@@ -1,12 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue';
-import { initialModel } from './config';
+import Vue from 'vue'
+import App from './App.vue'
+import { initialModel } from './config'
 
-  new Vue({
-    render:(h) => h(App, {
+new Vue({
+  render: (h) =>
+    h(App, {
       props: {
         initialModel,
       },
-    })
-  }).$mount('#plugin')
+    }),
+}).$mount('#plugin')
 
+// This error replaces another error which message is harder to understand and impossible to avoid util the issue https://github.com/storyblok/field-plugin/issues/107 has been resolved.
+throw new Error(
+  `This error can be safely ignored. It is caused by the legacy field plugin API. See issue https://github.com/storyblok/field-plugin/issues/107`,
+)
