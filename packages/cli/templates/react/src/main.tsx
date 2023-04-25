61724121e5eaeb5320cee889d17fc28fd6e01dcc
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-25 17:00:21 +0200

    Nicer error message from failing fieldtype-wrapper.js (#140)

--- packages/cli/templates/react/src/main.tsx
+++ packages/cli/templates/react/src/main.tsx
@@ -8,3 +8,8 @@ const rootNode = createRootElement()
 document.body.appendChild(rootNode)
 
 createRoot(rootNode).render(<App />)
+
+// This error replaces another error which message is harder to understand and impossible to avoid util the issue https://github.com/storyblok/field-plugin/issues/107 has been resolved.
+throw new Error(
+  `This error can be safely ignored. It is caused by the legacy field plugin API. See issue https://github.com/storyblok/field-plugin/issues/107`,
+)
