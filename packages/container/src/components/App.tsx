5aa3a61d23946b34f99058516e04bc5535c119b7
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-04-25 09:45:31 +0200

    EXT-1445: Persist Sandbox Origin in URL Query (#138)
    * feat(container): persisting the origin in the URL.
    
    * Moved usehooks-ts to dependencies.

--- packages/container/src/components/App.tsx
+++ packages/container/src/components/App.tsx
@@ -3,18 +3,32 @@ import { lightTheme, NotificationProvider } from '@storyblok/mui'
 import { Container, CssBaseline, ThemeProvider } from '@mui/material'
 import { FieldPluginContainerAppBar } from './FieldPluginContainerAppBar'
 import { FieldPluginContainer } from './FieldPluginContainer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6'
+import { QueryParamProvider } from 'use-query-params'
 
 export const App: FunctionComponent = () => (
   <ThemeProvider theme={lightTheme}>
     <CssBaseline />
     <FieldPluginContainerAppBar />
     <NotificationProvider>
-      <Container
-        maxWidth="md"
-        sx={{ py: 10 }}
-      >
-        <FieldPluginContainer />
-      </Container>
+      <BrowserRouter>
+        <QueryParamProvider adapter={ReactRouter6Adapter}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Container
+                  maxWidth="md"
+                  sx={{ py: 10 }}
+                >
+                  <FieldPluginContainer />
+                </Container>
+              }
+            />
+          </Routes>
+        </QueryParamProvider>
+      </BrowserRouter>
     </NotificationProvider>
   </ThemeProvider>
 )
