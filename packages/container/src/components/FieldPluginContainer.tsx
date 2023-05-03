78f8f9f63ffa4c88c7e165050d77377edd769f52
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-03 17:25:27 +0200

    feat(sandbox): Syntax highlighting and title for code blocks.

--- packages/container/src/components/FieldPluginContainer.tsx
+++ packages/container/src/components/FieldPluginContainer.tsx
@@ -24,13 +24,10 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
-  Box,
   Container,
-  IconButton,
-  Stack,
-  Tooltip,
+  Typography,
 } from '@mui/material'
-import { ChevronDownIcon, DeleteIcon, useNotifications } from '@storyblok/mui'
+import { ChevronDownIcon, useNotifications } from '@storyblok/mui'
 import { SchemaEditor } from './SchemaEditor'
 import { FieldTypePreview } from './FieldTypePreview'
 import { createContainerMessageListener } from '../dom/createContainerMessageListener'
@@ -38,7 +35,6 @@ import { useDebounce } from 'use-debounce'
 import { ValueView } from './ValueView'
 import { StringParam, useQueryParam, withDefault } from 'use-query-params'
 import { ObjectView } from './ObjectView'
-import { SectionHeader } from './SectionHeader'
 import { UrlView } from './UrlView'
 
 const uid = () => Math.random().toString(32).slice(2)
@@ -252,7 +248,7 @@ export const FieldPluginContainer: FunctionComponent = () => {
             expandIcon={<ChevronDownIcon />}
             sx={{ p: 0 }}
           >
-            <SectionHeader property="data.options">Options</SectionHeader>
+            <Typography variant="h3">Options</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <SchemaEditor
@@ -266,7 +262,7 @@ export const FieldPluginContainer: FunctionComponent = () => {
             expandIcon={<ChevronDownIcon />}
             sx={{ p: 0 }}
           >
-            <SectionHeader property="data.value">Value</SectionHeader>
+            <Typography variant="h3">Value</Typography>
           </AccordionSummary>
           <AccordionDetails sx={{ position: 'relative' }}>
             <ValueView
@@ -280,10 +276,15 @@ export const FieldPluginContainer: FunctionComponent = () => {
             expandIcon={<ChevronDownIcon />}
             sx={{ p: 0 }}
           >
-            <SectionHeader property="data">Data</SectionHeader>
+            <Typography variant="h3">Data</Typography>
           </AccordionSummary>
           <AccordionDetails sx={{ p: 0 }}>
             <ObjectView
+              title={
+                <Typography variant="caption">
+                  FieldPluginResponse.data
+                </Typography>
+              }
               output={{
                 value,
                 height,
