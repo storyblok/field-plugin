09e727d249fb707ea2b35f35e6ad5c7cf783d782
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-10 15:46:35 +0200

    feat(lib)!: `setPluginReady` Removed from `FieldPluginActions` (#179)
    * feat!(lib): `setPluginReady` Removed from `FieldPluginActions`
    
    * Removed old test

--- packages/field-plugin/src/createFieldPlugin/FieldPluginActions.ts
+++ packages/field-plugin/src/createFieldPlugin/FieldPluginActions.ts
@@ -3,7 +3,6 @@ import { Asset } from '../messaging'
 export type SetHeight = (heightPx: number) => void
 export type SetContent = (content: unknown) => void
 export type SetModalOpen = (isModal: boolean) => void
-export type SetPluginReady = () => void
 export type RequestContext = () => void
 export type SelectAsset = () => Promise<Asset>
 
@@ -11,7 +10,6 @@ export type FieldPluginActions = {
   setHeight: SetHeight
   setContent: SetContent
   setModalOpen: SetModalOpen
-  setPluginReady: SetPluginReady
   requestContext: RequestContext
   selectAsset: SelectAsset
 }
