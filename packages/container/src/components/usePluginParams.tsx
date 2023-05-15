656b301e3d6a15da90e685ac7b42f65a1a8a07da
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-15 20:51:33 +0200

    Replacing usage of iframe key with uid in params.

--- packages/container/src/components/usePluginParams.tsx
+++ packages/container/src/components/usePluginParams.tsx
@@ -1,14 +1,22 @@
-import { useMemo } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { PluginUrlParams } from '@storyblok/field-plugin'
 
+const randomString = () => Math.random().toString(32).slice(2)
+
 export const usePluginParams = () => {
-  return useMemo<PluginUrlParams>(
+  const [uid, setUid] = useState(randomString)
+  const randomizeUid = useCallback(() => setUid(randomString), [])
+  const pluginParams = useMemo<PluginUrlParams>(
     () => ({
-      uid: Math.random().toString(32).slice(2),
+      uid,
       host: window.location.host,
       secure: window.location.protocol === 'https:',
       preview: true,
     }),
-    [],
+    [uid],
   )
+  return {
+    pluginParams,
+    randomizeUid,
+  }
 }
