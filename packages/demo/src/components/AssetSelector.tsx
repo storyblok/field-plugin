02dcf652cd87ef0fce789107693a028bb32c578c
johannes-lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2023-05-19 07:59:21 +0200

    fix(demo): removed unused import

--- packages/demo/src/components/AssetSelector.tsx
+++ packages/demo/src/components/AssetSelector.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Box, Button, Divider, Stack, Typography } from '@mui/material'
+import { Box, Button, Stack, Typography } from '@mui/material'
 import { AssetIcon, DeleteIcon } from '@storyblok/mui'
 import { PluginComponent } from './FieldPluginDemo'
 import { Asset } from '@storyblok/field-plugin'
