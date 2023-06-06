62b04aaabd1d57812ddc73c5175ba3af9417c711
Eunjae Lee <hey@eunjae.dev>
2023-06-06 17:51:39 +0200

    move vue2 example to example folder

--- packages/cli/templates/vue2/src/App.vue
+++ packages/cli/templates/vue2/src/App.vue
@@ -3,13 +3,13 @@
     <template v-slot:loading><span>Loading...</span></template>
     <template v-slot:error><span>Error</span></template>
 
-    <FieldPlugin />
+    <FieldPluginExample />
   </FieldPluginProvider>
 </template>
 
 <script>
 import FieldPluginProvider from './components/FieldPluginProvider.vue'
-import FieldPlugin from './components/FieldPlugin.vue'
+import FieldPluginExample from './components/FieldPluginExample/index.vue'
 
 export default {
   components: {
