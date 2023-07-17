c569e8246b9e4f766697a155689a65ce93a765e5
Bibiana Sebestianova <b.sebestianova@gmail.com>
2023-07-17 13:46:45 +0200

    feat(common): added changes + adjusted copy-helpers in root to consider vue

--- scripts/copy-helpers.sh
+++ scripts/copy-helpers.sh
@@ -3,5 +3,5 @@
 mkdir ./packages/field-plugin/dist/{react,vue3}
 
 cp -r ./packages/helper-react/dist/* ./packages/field-plugin/dist/react/
-# cp -r ./packages/helper-vue3/dist/* ./packages/field-plugin/dist/vue3/
+cp -r ./packages/helper-vue3/dist/* ./packages/field-plugin/dist/vue3/
 
