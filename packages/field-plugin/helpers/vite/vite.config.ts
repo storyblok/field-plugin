5d0bceeb9b70e3673ff1a23df4524785577c9a53
Eunjae Lee <hey@eunjae.dev>
2023-08-04 17:53:41 +0200

    chore(cli): extract vite config as a submodule to the library

---
+++ packages/field-plugin/helpers/vite/vite.config.ts
@@ -0,0 +1,20 @@
+import { PluginOption, defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // there is a typing problem with the dts package
+  plugins: [dts() as unknown as PluginOption],
+  build: {
+    minify: false,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ViteHelper',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['querystring'],
+    },
+  },
+})
