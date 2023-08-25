a4f1054fd7995df986ef20cdb88a9b542a9b9640
Eunjae Lee <hey@eunjae.dev>
2023-08-25 14:31:07 +0200

    remove UpdaterFunctionDemo from demo app

--- packages/demo/src/components/NonModalView.tsx
+++ packages/demo/src/components/NonModalView.tsx
@@ -1,19 +1,17 @@
-import { Paper, Stack } from '@mui/material'
-import { ModalToggle } from './ModalToggle'
-import { ValueMutator } from './ValueMutator'
-import { HeightChangeDemo } from './HeightChangeDemo'
-import { UpdaterFunctionDemo } from './UpdaterFunctionDemo'
-import { AssetSelector } from './AssetSelector'
-import { ContextRequester } from './ContextRequester'
-import { PluginComponent } from './FieldPluginDemo'
-import { LanguageView } from './LanguageView'
+import { Paper, Stack } from "@mui/material";
+import { ModalToggle } from "./ModalToggle";
+import { ValueMutator } from "./ValueMutator";
+import { HeightChangeDemo } from "./HeightChangeDemo";
+import { AssetSelector } from "./AssetSelector";
+import { ContextRequester } from "./ContextRequester";
+import { PluginComponent } from "./FieldPluginDemo";
+import { LanguageView } from "./LanguageView";
 
 export const NonModalView: PluginComponent = (props) => (
   <Paper>
     <Stack gap={6}>
       <ModalToggle {...props} />
       <ValueMutator {...props} />
-      <UpdaterFunctionDemo {...props} />
       <ModalToggle {...props} />
       <AssetSelector {...props} />
       <ContextRequester {...props} />
@@ -21,4 +19,4 @@ export const NonModalView: PluginComponent = (props) => (
       <LanguageView {...props} />
     </Stack>
   </Paper>
-)
+);
