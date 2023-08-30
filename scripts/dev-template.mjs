7632dab09e51cbd6a08203c495fa02bc1e0dacb4
Eunjae Lee <hey@eunjae.dev>
2023-08-30 15:19:54 +0200

    add comment

--- scripts/dev-template.mjs
+++ scripts/dev-template.mjs
@@ -1,5 +1,19 @@
 #!/usr/bin/env zx
 
+/*
+  It's totally okay to run `yarn workspace field-plugin-react-template dev`.
+  However, if you want to test changes in `@storyblok/field-plugin` library (under `packages/field-plugin/` folder),
+  you must run `yarn build:lib` to reflect the change in the react template.
+  To avoid this inefficiency, we can add `resolve.alias` config into `vite.config.ts` of each template.
+  This will resolve the source code of the library instead of its bundle output.
+
+  However, we don't want to put `resolve.alias` config inside of the templates,
+  because the CLI uses the templates to create projects for users, and we don't want this config there.
+  To avoid having this config directly inside vite config of the templates,
+  this script creates a temporary config that extends the real config, adding the extra `resolve.alias` config.
+  https://github.com/storyblok/field-plugin/pull/264
+*/
+
 import fs from 'fs/promises'
 import path from 'path'
 
@@ -15,9 +29,9 @@ file = file.replace(
     alias: [{
       find: /^@storyblok\\/field-plugin$/,
       replacement: '${path.resolve(
-        __dirname,
-        '../packages/field-plugin/src/index.ts',
-      )}'
+    __dirname,
+    '../packages/field-plugin/src/index.ts',
+  )}'
     }]
   },
   plugins:`,
