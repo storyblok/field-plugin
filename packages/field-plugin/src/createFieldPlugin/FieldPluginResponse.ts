cf254bb93f7616248be2bfe1a1b8f1701e25fe99
Eunjae Lee <hey@eunjae.dev>
2023-09-08 10:49:45 +0200

    fix broken tests

--- packages/field-plugin/src/createFieldPlugin/FieldPluginResponse.ts
+++ packages/field-plugin/src/createFieldPlugin/FieldPluginResponse.ts
@@ -2,21 +2,21 @@ import { FieldPluginData } from './FieldPluginData'
 import { FieldPluginActions } from './FieldPluginActions'
 
 export type FieldPluginResponse<Content> =
-    | {
-        type: 'loading'
-        error?: never
-        data?: never
-        actions?: never
+  | {
+      type: 'loading'
+      error?: never
+      data?: never
+      actions?: never
     }
-    | {
-        type: 'error'
-        error: Error
-        data?: never
-        actions?: never
+  | {
+      type: 'error'
+      error: Error
+      data?: never
+      actions?: never
     }
-    | {
-        type: 'loaded'
-        error?: never
-        data: FieldPluginData<Content>
-        actions: FieldPluginActions<Content>
+  | {
+      type: 'loaded'
+      error?: never
+      data: FieldPluginData<Content>
+      actions: FieldPluginActions<Content>
     }
