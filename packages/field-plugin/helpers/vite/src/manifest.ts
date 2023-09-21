2319e6e6f35e039f29ac78c24f144890ad71784e
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-09-21 15:38:04 -0300

    add manifest schema validation

--- packages/field-plugin/helpers/vite/src/manifest.ts
+++ packages/field-plugin/helpers/vite/src/manifest.ts
@@ -21,8 +21,39 @@ export const load = (): Manifest => {
   try {
     const content: string = readFileSync(MANIFEST_FULL_PATH, 'utf8')
 
-    return JSON.parse(content) as Manifest
+    const manifest: Manifest = JSON.parse(content)
+
+    validateSchema(manifest)
+
+    return manifest
   } catch (err: any) {
     throw new Error('Error while loading the manifest file: ' + err.message)
   }
+}
+
+const validateSchema = (manifest: Manifest): void => {
+  const allowedProperties = ['options']
+
+  if (typeof manifest !== 'object') {
+    throw new Error(`The manifest should be an object`)
+  }
+
+  Object.keys(manifest).forEach((prop) => {
+    if (!allowedProperties.includes(prop)) {
+      throw new Error(`The property ${prop} is not allowed`)
+    }
+  })
+
+  if (manifest.options !== undefined && !Array.isArray(manifest.options)) {
+    throw new Error(`When declared, the 'options' property should be an array`)
+  }
+
+  manifest.options?.forEach((o) => {
+    if (!('name' in o && 'value' in o)) {
+      throw new Error(
+        `Some of the defined 'options' are invalid. ` +
+          `Please, make sure they contain a 'name' and 'value' properties`,
+      )
+    }
+  })
 }
