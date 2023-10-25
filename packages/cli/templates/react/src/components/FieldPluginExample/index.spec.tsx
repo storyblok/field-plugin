161e6eb3038261935dec5e2d1a9f0895140a574a
Philipp Litzenberger <philipp@bendcircular.com>
2023-10-25 17:16:56 +0200

    feat(template): setup vitest for react template (#288)

---
+++ packages/cli/templates/react/src/components/FieldPluginExample/index.spec.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react'
+import { describe, test, expect, vi } from 'vitest'
+
+import FieldPlugin from '.'
+import { useFieldPlugin } from '@storyblok/field-plugin/react'
+import { FieldPluginResponse } from '@storyblok/field-plugin'
+
+vi.mock('@storyblok/field-plugin/react')
+
+const fieldPluginDefault: FieldPluginResponse<number | undefined> = {
+  type: 'loaded',
+  data: {
+    isModalOpen: false,
+    content: undefined,
+    options: {},
+    spaceId: undefined,
+    storyLang: '',
+    story: {
+      content: {},
+    },
+    storyId: undefined,
+    blockUid: undefined,
+    token: undefined,
+    uid: '',
+  },
+  actions: {
+    setContent: vi.fn(),
+    setModalOpen: vi.fn(),
+    requestContext: vi.fn(),
+    selectAsset: vi.fn(),
+  },
+}
+
+describe('FieldPluginExammple', () => {
+  test('should work', () => {
+    vi.mocked(useFieldPlugin).mockReturnValue(fieldPluginDefault)
+    render(<FieldPlugin />)
+    const headline = screen.getByText('Field Value')
+    expect(headline).toBeInTheDocument()
+  })
+})
