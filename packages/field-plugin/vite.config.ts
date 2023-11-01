79ff91804a2facd4335adaa44698930b797aecde
Demetrius Feijóo <demetrius.feijoo@storyblok.com>
2023-11-01 16:51:37 -0300

    break only helpers

--- packages/field-plugin/vite.config.ts
+++ packages/field-plugin/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url'
 import dts from 'vite-plugin-dts'
+import { copyFileSync } from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     dts({
       insertTypesEntry: true,
+      compilerOptions: {
+        outFile: 'index.js',
+      },
+      afterBuild: () => {
+        copyFileSync('dist/index.d.ts', 'dist/index.d.mts')
+      },
     }),
   ],
   build: {
@@ -14,7 +21,7 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
       name: 'FieldPlugin',
-      fileName: 'field-plugin',
+      fileName: 'index',
       formats: ['es', 'cjs'],
     },
     emptyOutDir: false,
