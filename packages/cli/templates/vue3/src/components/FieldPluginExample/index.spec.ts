a25ba1ae8fb3eeecc9d091a60874b95003858cf1
Eunjae Lee <hey@eunjae.dev>
2023-12-06 11:04:36 +0100

    add comments

--- packages/cli/templates/vue3/src/components/FieldPluginExample/index.spec.ts
+++ packages/cli/templates/vue3/src/components/FieldPluginExample/index.spec.ts
@@ -8,8 +8,12 @@ import FieldPlugin from './index.vue'
 describe('FieldPluginExample', async () => {
   test('should render the component', async () => {
     const { cleanUp } = setupFieldPlugin()
-    render(FieldPlugin)
-    await nextTick()
+    // We wait for `nextTick` because it takes a moment
+    // for `plugin.type` to become `loaded`.
+    // After that, we can start testing the behaviors.
+    render(FieldPlugin) // plugin.type === 'loading'
+    await nextTick() // plugin.type === 'loaded'
+
     const headline = screen.getByText('Field Value')
     expect(headline).toBeInTheDocument()
     cleanUp()
@@ -18,8 +22,12 @@ describe('FieldPluginExample', async () => {
   test('should increase the counter', async () => {
     const { cleanUp } = setupFieldPlugin()
     const user = userEvent.setup()
-    render(FieldPlugin)
-    await nextTick()
+    // We wait for `nextTick` because it takes a moment
+    // for `plugin.type` to become `loaded`.
+    // After that, we can start testing the behaviors.
+    render(FieldPlugin) // plugin.type === 'loading'
+    await nextTick() // plugin.type === 'loaded'
+
     expect(screen.getByTestId('count').textContent).toEqual('0')
     await user.click(screen.getByText('Increment'))
     expect(screen.getByTestId('count').textContent).toEqual('1')
