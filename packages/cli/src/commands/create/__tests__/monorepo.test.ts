ba897a48c4673f41659c707972bc2dc717871278
Samuel Höra <samuel@hoera.dev>
2024-01-11 15:07:49 +0100

    fix(cli): set specific version of pnpm for monorepos (#330)
    * fix: set specific version of pnpm for monorepos
    
    * feat: skip yarn files for non yarn monorepos
    
    * fix: update test
    
    * fix: handle yarn files more specifically
    
    * fix: skip logic not catching all relevant yarn files
    
    ---------
    
    Co-authored-by: Eunjae Lee <hey@eunjae.dev>

--- packages/cli/src/commands/create/__tests__/monorepo.test.ts
+++ packages/cli/src/commands/create/__tests__/monorepo.test.ts
@@ -4,20 +4,23 @@ import { add } from '../../add'
 import { createMonorepo } from '../monorepo'
 import { randomString } from '../../../utils'
 import { vi } from 'vitest'
+import { CreateMonorepoArgs } from '../types'
 
 vi.mock('../../add')
 
-const createMonorepoSilently = async () => {
+const createMonorepoSilently = async (
+  options?: Partial<Pick<CreateMonorepoArgs, 'packageManager' | 'template'>>,
+) => {
   const repoName = randomString()
   const pluginName = randomString()
   const dir = tempDir
 
   await createMonorepo({
     dir,
-    packageManager: 'npm',
+    packageManager: options?.packageManager || 'npm',
     repoName,
     pluginName,
-    template: 'react',
+    template: options?.template || 'react',
   })
 
   return { repoName, pluginName, dir }
@@ -36,6 +39,36 @@ describe('monorepo', () => {
       dir: `${dir}/${repoName}/packages`,
       name: pluginName,
       packageManager: 'npm',
+      structure: 'monorepo',
+      template: 'react',
+    })
+
+    expect(await readdir(`${dir}/${repoName}`)).toMatchInlineSnapshot(`
+      [
+        ".env.local.example",
+        ".git",
+        ".gitignore",
+        ".nvmrc",
+        ".prettierrc",
+        "package.json",
+        "packages",
+        "tsconfig.base.json",
+        "tsconfig.node.base.json",
+        "vite-env.d.ts",
+      ]
+    `)
+  })
+
+  it('creates a monorepo structure with yarn', async () => {
+    const { repoName, pluginName, dir } = await createMonorepoSilently({
+      packageManager: 'yarn',
+    })
+
+    expect(add).toHaveBeenCalledTimes(1)
+    expect(add).toHaveBeenCalledWith({
+      dir: `${dir}/${repoName}/packages`,
+      name: pluginName,
+      packageManager: 'yarn',
       structure: 'monorepo',
       template: 'react',
     })
